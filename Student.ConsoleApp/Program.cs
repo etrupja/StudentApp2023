ac1a6c694da95c7a83f3ba647afa7783b1a62c04
“Ervis <ervistrupja@outlook.com>
2023-10-23 11:49:40 +0200

    Initial commit

---
+++ Student.ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Student.ConsoleApp;
+
+Console.WriteLine("----- START -----");
+
+var nonStaticClass = new Student.ConsoleApp.Student();
+var nonStaticClassFullName = nonStaticClass.GetFullName("Emri", "Mbiemri");
+Console.WriteLine($"Full name (non-static) = {nonStaticClassFullName}");
+
+var staticClassFullName = StudentStatic.GetFullName("Emri", "Mbiemri");
+Console.WriteLine($"Full name (static) = {staticClassFullName}");
+
+
+
+var student = new Student.ConsoleApp.Student()
+{
+    Id = 1,
+    FirstName = "Student",
+    LastName = "Fullname",
+    Email = "student@email.com",
+    EnrollmentYear = 2022,
+    DOB = DateTime.UtcNow
+};
+
+//Console.WriteLine($"Student FullName: {student.FirstName} {student.LastName}");
+//Console.WriteLine($"Student Email: {student.Email}");
+
+//var studentOther = new Student.ConsoleApp.Student()
+//{
+//    Id = 2,
+//    FirstName = "Other",
+//    LastName = "Fullname",
+//    Email = "student@other.com",
+//    EnrollmentYear = 2021,
+//    DOB = DateTime.UtcNow
+//};
+
+Console.WriteLine("----- END -----");
---
+++ Student.ConsoleApp/StudentStatic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.ConsoleApp
+{
+    public static class StudentStatic
+    {
+        public static string FirstName { get; set; }
+        public static string LastName { get; set; }
+
+        public static string GetFullName(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+    }
+}
